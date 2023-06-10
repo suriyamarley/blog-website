1b3a146b08444ce00963da556b83b0d152fe6618
SURIYA <suriyamarley70@gmail.com>
2023-06-09 21:57:47 -0700

    changes complete

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBn0X53ww-SVeidvXp5PgYB62mpHTxB0ew",
-  authDomain: "react-blog-d9148.firebaseapp.com",
-  projectId: "react-blog-d9148",
-  storageBucket: "react-blog-d9148.appspot.com",
-  messagingSenderId: "1078737357999",
-  appId: "1:1078737357999:web:5ee53cb2cc669405fe185a",
-  measurementId: "G-5KZYTLREWG"
+  apiKey: "AIzaSyBzKWXCaAw-gIa3EK8YkX1OOcZMT_0QpRg",
+  authDomain: "suriya-blog.firebaseapp.com",
+  projectId: "suriya-blog",
+  storageBucket: "suriya-blog.appspot.com",
+  messagingSenderId: "437927390737",
+  appId: "1:437927390737:web:a639c5261979b47d79cbb0",
+  measurementId: "G-SRD9YFM8D1"
 };
 
 const app = initializeApp(firebaseConfig);
